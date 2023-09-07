821970cff5a48ab1e7c6759aceae5f647a0e734f
Rubayet-billah <rubayetbillah909@gmail.com>
2023-09-07 15:54:53 +0600

    redux installed and set up

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,14 @@ import "./index.css";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 AOS.init();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/features/api/apiSlice.js
@@ -0,0 +1,21 @@
+// api.js
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }), // Configure your API base URL
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "posts",
+    }),
+    createPost: builder.mutation({
+      query: (newPost) => ({
+        url: "posts",
+        method: "POST",
+        body: newPost,
+      }),
+    }),
+  }),
+});
+
+export const { useGetPostsQuery, useCreatePostMutation } = api;
+export default api;
