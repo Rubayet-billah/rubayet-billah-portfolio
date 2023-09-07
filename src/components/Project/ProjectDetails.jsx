9aade8ad1933398b73947b7118c8dfc3f00af66d
Rubayet-billah <rubayetbillah909@gmail.com>
2023-09-07 15:11:18 +0600

    fix

--- src/components/Project/ProjectDetails.jsx
+++ src/components/Project/ProjectDetails.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { BsGithub, BsGlobeAsiaAustralia } from "react-icons/bs";
 import { CiShare1 } from "react-icons/ci";
