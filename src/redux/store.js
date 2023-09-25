ea47f8c224603e960d530c08f89228f92ce29f04
Rubayet-billah <rubayetbillah909@gmail.com>
2023-09-25 20:24:59 +0600

    firebase config added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import projectSlice from "./features/project/projectSlice";
 import api from "./features/api/apiSlice";
+import authSlice from "./features/auth/authSlice";
 
 const store = configureStore({
   reducer: {
+    auth: authSlice,
     project: projectSlice,
     [api.reducerPath]: api.reducer,
   },
