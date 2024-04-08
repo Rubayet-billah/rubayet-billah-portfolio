1bc7f74453472cb1806816c19109bab0a8bdf34e
Rubayet-billah <rubayetbillah909@gmail.com>
2024-04-08 22:49:05 +0600

    contact fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -29,18 +29,18 @@ const About = () => {
     <PageContainer id="about">
       <PagesHeading>About Me</PagesHeading>
       <div className="container mx-auto">
-        <div className=" shadow-lg rounded-lg">
-          <div className="flex flex-col md:flex-row items-center md:space-x-4">
-            <div className="md:w-1/3 mb-4">
+        <div className="rounded-lg shadow-lg ">
+          <div className="flex flex-col items-center md:flex-row md:space-x-4">
+            <div className="mb-4 md:w-1/3">
               {/* Image Section */}
               <img
                 src={profileImg}
                 alt="Your Name"
-                className="w-full h-auto rounded-full max-w-xs mx-auto"
+                className="w-full h-auto max-w-xs mx-auto rounded-full"
               />
             </div>
             <div className="md:w-2/3">
-              <h1 className="text-3xl font-semibold uppercase mb-4">
+              <h1 className="mb-4 text-3xl font-semibold uppercase">
                 Hi There
               </h1>
               <p className="mb-4">
@@ -48,30 +48,30 @@ const About = () => {
                 front-end and back-end technologies. I love building web
                 applications that are both functional and visually appealing.
               </p>
-              <section className="grid grid-cols-1 md:grid-cols-2 gap-5">
+              <section className="grid grid-cols-1 gap-5 md:grid-cols-2">
                 <div className="flex items-center">
-                  <FiUser className="text-teal-300 mr-2" />
+                  <FiUser className="mr-2 text-teal-300" />
                   <div>
                     <h3 className="font-bold text-teal-300 uppercase">Name</h3>
                     <p>{fullName}</p>
                   </div>
                 </div>
                 <div className="flex items-center">
-                  <FiMail className="text-teal-300 mr-2" />
+                  <FiMail className="mr-2 text-teal-300" />
                   <div>
                     <h3 className="font-bold text-teal-300 uppercase">Email</h3>
                     <p>{email}</p>
                   </div>
                 </div>
                 <div className="flex items-center">
-                  <FiPhone className="text-teal-300 mr-2" />
+                  <FiPhone className="mr-2 text-teal-300" />
                   <div>
                     <h3 className="font-bold text-teal-300 uppercase">Phone</h3>
                     <p>{phoneNumber}</p>
                   </div>
                 </div>
                 <div className="flex items-center">
-                  <FiLinkedin className="text-teal-300 mr-2" />
+                  <FiLinkedin className="mr-2 text-teal-300" />
                   <div>
                     <h3 className="font-bold text-teal-300 uppercase">
                       LinkedIn
@@ -89,21 +89,22 @@ const About = () => {
                   </div>
                 </div>
                 <div className="flex items-center">
-                  <FiBook className="text-teal-300 mr-2" />
+                  <FiBook className="mr-2 text-teal-300" />
                   <div>
                     <h3 className="font-bold text-teal-300 uppercase">
                       Education
                     </h3>
                     <p>Bachelor's in Physics</p>
                     <p>BSMR Science and Technology University</p>
-                    <p>Graduated: N/A</p>
+                    <p>Gopalganj, Bangladesh</p>
+                    <p>2019 - Present</p>
                   </div>
                 </div>
               </section>
-              <hr className="border border-gray-600 my-5" />
-              <section className="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-3">
+              <hr className="my-5 border border-gray-600" />
+              <section className="flex flex-col space-y-3 md:flex-row md:space-y-0 md:space-x-3">
                 <button
-                  className="flex items-center justify-center px-4 py-2 text-teal-300 border border-teal-300 hover:bg-teal-300 hover:text-white rounded-md transition duration-300 ease-in-out w-full md:w-auto"
+                  className="flex items-center justify-center w-full px-4 py-2 text-teal-300 transition duration-300 ease-in-out border border-teal-300 rounded-md hover:bg-teal-300 hover:text-white md:w-auto"
                   onClick={() => {
                     // Add your contact action here
                   }}
@@ -112,7 +113,7 @@ const About = () => {
                   Contact Me
                 </button>
                 <button
-                  className="flex items-center justify-center px-4 py-2 text-teal-300 border border-teal-300 hover:bg-teal-300 hover:text-white rounded-md transition duration-300 ease-in-out w-full md:w-auto"
+                  className="flex items-center justify-center w-full px-4 py-2 text-teal-300 transition duration-300 ease-in-out border border-teal-300 rounded-md hover:bg-teal-300 hover:text-white md:w-auto"
                   onClick={() => {
                     // Add your download CV action here
                   }}
